afc260f567de1f55ea419b9ef8989c0d5af67424
Deadreyo <ahmedatwa866@yahoo.com>
2022-04-16 21:13:09 +0200

    up

--- nodejs practice/src/main.ts
+++ nodejs practice/src/main.ts
@@ -1,1 +1,25 @@
-console.log("hello World")
+import { writeFile } from 'fs';
+import fetch from 'node-fetch'
+
+export default async function test() {
+    const keyAPI = 'removed'
+    const fileId = '1b3o_r6uGRyFWRRKyTH86e9hcaGcqqm7i'
+    const link = `https://www.googleapis.com/drive/v3/files/${fileId}?key=${keyAPI}&alt=media`;
+    
+    try{
+        const data =  await fetch(link);
+        const status = await data.statusText
+        const buffer = await data.buffer()
+        console.log(data);
+        console.log(`status: `,status);
+        // console.log(`text: `, data.text);
+        let filePath = `${__dirname}/../file.pdf`
+        writeFile(filePath, buffer, () => {
+            console.log(filePath);
+    })
+    } catch(e) {
+        console.log(`ERROR: `,e)
+    }
+}
+
+test();
